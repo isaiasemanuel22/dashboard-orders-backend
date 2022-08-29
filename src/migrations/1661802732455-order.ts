817b31ed4c877edc430de42560cba826a5c07deb
Isaias <isaiasemanuelcalfin@hotmail.com>
2022-08-29 16:56:19 -0300

    migration

--- src/migrations/1661802732455-order.ts
+++ src/migrations/1661802732455-order.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class updatePostTable1661802155523 implements MigrationInterface {
-    name = 'updatePostTable1661802155523'
+export class order1661802732455 implements MigrationInterface {
+    name = 'order1661802732455'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`order\` CHANGE \`name\` \`name\` varchar(255) NULL`);
